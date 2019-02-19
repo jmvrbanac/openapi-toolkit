b102e59c884c3820ff0dff4c55ded8fd2f3e49b2
John Vrbanac <john.vrbanac@linux.com>
2019-02-19 13:54:43 -0600

    Adding indent mako filter

--- openapi_toolkit/preprocessor.py
+++ openapi_toolkit/preprocessor.py
@@ -1,5 +1,27 @@
-from mako.template import Template
+import inspect
+import re
+
+from mako.template import Template, ModuleInfo, _get_module_info
 from mako.lookup import TemplateLookup
+
+
+def indent(text, *args):
+    """Indent filter for Mako that matches template indention"""
+    _, module_name, line_no, *_ = inspect.stack()[1]
+    module_info = _get_module_info(module_name)
+    module_source, template_source = module_info.code, module_info.source
+
+    source_map = ModuleInfo.get_module_source_metadata(
+        module_source,
+        full_line_map=True
+    )
+
+    line_map = source_map['full_line_map']
+    template_ln_no = line_map[line_no - 1]
+    template_line = template_source.split('\n')[template_ln_no - 1]
+
+    indent = re.match('[ \t]*', template_line).group(0)
+    return indent.join(x for x in text.splitlines(keepends=True))
 
 
 class MakoPreprocessor(object):
@@ -10,6 +32,7 @@ class MakoPreprocessor(object):
             self.lookup = TemplateLookup(
                 directories=directories,
                 module_directory=module_directory,
+
             )
 
     def handle(self, raw):
